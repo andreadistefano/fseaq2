a0d0718691500e31c04db289a5e2948e5d689086
Luca Di Stefano <lucadistefano90@gmail.com>
2014-11-23 17:34:55 +0100

    Distintivo belongs_to Branca

--- src/app/models/distintivo.rb
+++ src/app/models/distintivo.rb
@@ -1,3 +1,5 @@
 class Distintivo < ActiveRecord::Base
+	belongs_to :branca, inverse_of: :distintivi
+
 	validates :livello, uniqueness: {scope: :branca}, unless: :specialita
 end
---
+++ src/test/controllers/cerimonie_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class CerimonieControllerTest < ActionController::TestCase
+  setup do
+    @cerimonia = cerimonia(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:cerimonia)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create cerimonia" do
+    assert_difference('Cerimonia.count') do
+      post :create, cerimonia: { data: @cerimonia.data, distintivo_id: @cerimonia.distintivo_id, socio_id: @cerimonia.socio_id }
+    end
+
+    assert_redirected_to cerimonia_path(assigns(:cerimonia))
+  end
+
+  test "should show cerimonia" do
+    get :show, id: @cerimonia
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @cerimonia
+    assert_response :success
+  end
+
+  test "should update cerimonia" do
+    patch :update, id: @cerimonia, cerimonia: { data: @cerimonia.data, distintivo_id: @cerimonia.distintivo_id, socio_id: @cerimonia.socio_id }
+    assert_redirected_to cerimonia_path(assigns(:cerimonia))
+  end
+
+  test "should destroy cerimonia" do
+    assert_difference('Cerimonia.count', -1) do
+      delete :destroy, id: @cerimonia
+    end
+
+    assert_redirected_to cerimonie_path
+  end
+end
