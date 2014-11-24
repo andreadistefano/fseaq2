63ce45fa6aac6a28012659ed2fae531879dedfb3
Andrea Di Stefano <andrea.distefano@outlook.com>
2014-11-24 23:49:31 +0100

    Validazione censimento anno scout in Cerimonia

--- src/app/models/cerimonia.rb
+++ src/app/models/cerimonia.rb
@@ -1,4 +1,14 @@
 class Cerimonia < ActiveRecord::Base
   belongs_to :socio
   belongs_to :distintivo
+
+  validate :socio_censito_in_branca
+
+  def socio_censito_in_branca
+    anno = AnnoScout.new(data).to_i
+    if socio.censimenti.anno(anno).empty? or
+      socio.censimenti.anno(anno).first.unita.branca != distintivo.branca
+      errors.add(:anno, "Socio non censito in questa branca!")
+    end
+  end
 end
