310685df15adf427aee8fa81bcd328c2cd0be2bd
Luca Di Stefano <lucadistefano90@gmail.com>
2014-11-24 19:23:48 +0100

    Notizie_controller: aggiornamento Unità

--- src/app/controllers/notizie_controller.rb
+++ src/app/controllers/notizie_controller.rb
@@ -73,7 +73,6 @@ class NotizieController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notizia_params
-      params[:notizia]
-      params.require(:notizia).permit(:title, :text)
+      params.require(:notizia).permit(:title, :text, { :unita_ids => [] })
     end
 end
